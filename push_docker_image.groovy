3fd91f6b1fa669fffbe43b54d2b9a203baeaba3b
ahmed mostafa saad <119954278+ahmedmostafa56389@users.noreply.github.com>
2024-06-19 18:42:12 +0300

    Update push_docker_image.groovy

--- push_docker_image.groovy
+++ push_docker_image.groovy
@@ -4,9 +4,6 @@ def call(String imageName, String dockerHubCredentialsID) {
     withCredentials([usernamePassword(credentialsId: dockerHubCredentialsID, usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
         sh "docker login -u ${USERNAME} -p ${PASSWORD}"
     }
-    
-    echo "build"
-    sh "docker build -t ${imageName} ."
     echo "push to docker hub"
     sh "docker push ${imageName}"
 }
