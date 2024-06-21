f245741d6444ca0ce12fd870c3f6c755c0e45cc8
ahmed mostafa saad <119954278+ahmedmostafa56389@users.noreply.github.com>
2024-06-21 13:13:41 +0300

    Update build_docker_image.groovy

--- vars/build_docker_image.groovy
+++ vars/build_docker_image.groovy
@@ -1,6 +1,6 @@
-#!/bin/env groovy
+#!usr/bin/env groovy
 def call(String IMAGE_NAME) {
       echo "build"
-    sh "docker build -t ${IMAGE_NAME} ."
-    }
+      sh "docker build -t ${IMAGE_NAME} ."
+}
     
