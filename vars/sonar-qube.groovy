8fb9061eae2cb9723bc87c3ee1a792747874d69a
ahmed mostafa saad <119954278+ahmedmostafa56389@users.noreply.github.com>
2024-06-21 23:35:03 +0300

    Update sonar-qube.groovy

--- vars/sonar-qube.groovy
+++ vars/sonar-qube.groovy
@@ -5,7 +5,7 @@ def call() {
   withSonarQubeEnv(credentialsId: 'sonar') {
         echo "Running SonarQube Analysis..."
         sh ''' 
-        $SCANNER_HOME/bin/sonar -Dsonar.projectName=ahmed \
+        $SCANNER_HOME/bin/sonar-scanner -Dsonar.projectName=ahmed \
         -Dsonar.java.binaries=. \
         -Dsonar.projectKey=ahmed 
         '''
