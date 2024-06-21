3872b759c8abb2eef04312b101b5f081e8fc302d
ahmed mostafa saad <119954278+ahmedmostafa56389@users.noreply.github.com>
2024-06-21 23:41:27 +0300

    Update sonar-qube.groovy

--- vars/sonar-qube.groovy
+++ vars/sonar-qube.groovy
@@ -1,13 +1,25 @@
 #!/usr/bin/env groovy
 
 def call() {
-  echo "sonar"
-  withSonarQubeEnv(credentialsId: 'sonar') {
-        echo "Running SonarQube Analysis..."
-        sh ''' 
-        $SCANNER_HOME/bin/sonar-scanner -Dsonar.projectName=ahmed \
-        -Dsonar.java.binaries=. \
-        -Dsonar.projectKey=ahmed 
-        '''
-  }
+    echo "Running SonarQube Analysis..."
+
+    // Define your SonarQube server configuration credentials ID
+    def sonarqubeCredentialsId = 'sonar'
+
+    // Define other properties for SonarQube analysis
+    def projectName = 'ahmed'
+    def projectKey = 'ahmed'
+
+    // Run withSonarQubeEnv to inject SonarQube environment variables
+    withSonarQubeEnv(credentialsId: sonarqubeCredentialsId) {
+        // Assuming you have a SonarQube tool installation named 'SonarQubeScanner'
+        def scannerHome = tool name: 'sonar-scanner', type: 'hudson.plugins.sonar.SonarRunnerInstallation'
+        
+        sh """
+        ${scannerHome}/bin/sonar-scanner \
+          -Dsonar.projectName=${projectName} \
+          -Dsonar.projectKey=${projectKey} \
+          -Dsonar.java.binaries=.
+        """
+    }
 }
